1467d86e0ac96cf499cebd641d44f3899d938b0c
Glenn Tigas <glenn.tigas@gmail.com>
2017-11-27 12:51:15 -0500

    Add ability to switch between control schemes

--- bundle.js
+++ bundle.js
@@ -471,6 +471,12 @@ document.addEventListener("DOMContentLoaded", () =>{
     Object(__WEBPACK_IMPORTED_MODULE_3__binders__["a" /* default */])(game);
     game.play()
   }
+  $(".open-controls").on("click", () => {
+    $(".controls").toggleClass("hidden")
+  })
+  $(".close-controls").on("click", () => {
+    $(".controls").addClass("hidden")
+  })
 });
 
 
@@ -712,54 +718,88 @@ class PreviewPiece {
 "use strict";
 const bindKeys = (game) => {
   let down = false
-  document.addEventListener("keypress", (e) => {
-    switch (e.key) {
-      case 'a':
-        // game.move('left')
-        break;
-      case 'd':
-        // game.move('right')
-        break;
+  const configs = {
+    "1" : (e) => {
+      switch (e.keyCode) {
+        case 37:
+          game.move('left')
+          break;
+        case 32:
+          game.hardDrop()
+          break;
+        case 39:
+          game.move('right')
+          break;
+        case 40:
+          game.move('down')
+          break;
+        case 90:
+          if (down) return
+          down = true
+          game.rotate('left')
+          break;
+        case 88:
+          if (down) return
+          down = true
+          game.rotate('right')
+          break;
+        case 67:
+          game.holdPiece();
+          break;
+        case 82:
+          game.restart();
+          break;
+      }
+    },
+    "2": (e) => {
+      switch (e.keyCode) {
+        case 65:
+          game.move('left')
+          break;
+        case 87:
+          game.hardDrop()
+          break;
+        case 68:
+          game.move('right')
+          break;
+        case 83:
+          game.move('down')
+          break;
+        case 39:
+          if (down) return
+          down = true
+          game.rotate('left')
+          break;
+        case 37:
+          if (down) return
+          down = true
+          game.rotate('right')
+          break;
+        case 16:
+          game.holdPiece();
+          break;
+        case 82:
+          game.restart();
+          break;
+      }
     }
-  })
-  document.addEventListener("keydown", (e) => {
-    switch (e.keyCode) {
-      case 65:
-        game.move('left')
-        break;
-      case 87:
-        game.hardDrop()
-        break;
-      case 68:
-        game.move('right')
-        break;
-      case 83:
-        game.move('down')
-        break;
-      case 39:
-        if (down) return
-        down = true
-        game.rotate('left')
-        break;
-      case 37:
-        if (down) return
-        down = true
-        game.rotate('right')
-        break;
-      case 16:
-        game.holdPiece();
-        break;
-      case 82:
-        game.restart();
-        break;
-    }
-  })
+  }
 
+
+
+  let currentConfig = configs["1"]
+  document.addEventListener("keydown", currentConfig)
+  $(".close-controls").on("click", () => {
+    document.removeEventListener("keydown", currentConfig)
+    currentConfig = configs[$("input[name='config']:checked").val()]
+    document.addEventListener("keydown", currentConfig)
+    $(".controls").addClass("hidden")
+  })
   document.addEventListener('keyup', function () {
     down = false;
   }, false);
-
 }
+
 
 /* harmony default export */ __webpack_exports__["a"] = (bindKeys);
 
